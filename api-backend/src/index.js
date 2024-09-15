ecff080ea783a548a8550a6b4a883fb5f0fee86b
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-09-15 23:33:08 +0530

    Update

---
+++ api-backend/src/index.js
@@ -0,0 +1,6 @@
+// sample routes for api server for judge engine stores case information ,files ,etc using express js
+
+
+
+
+
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int x=0;
+// Initially, x = 0  in main memory.
+
+//  running on P1
+P1: x = 1;
+
+// running on P2(printing x)
+P2: std::cout<<(x);
+}
